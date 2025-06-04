6db2c93216964e5da7e9cfaf2043b3ee316d3b19
Udara Nalawansa <udarakavishka13@gmail.com>
2025-06-04 23:36:53 +0530

    feat: add JsonIgnore annotation to prevent serialization of employees list in Department model

--- sampleapp/src/main/java/com/system/sampleapp/model/Department.java
+++ sampleapp/src/main/java/com/system/sampleapp/model/Department.java
@@ -3,6 +3,8 @@ package com.system.sampleapp.model;
 import java.sql.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -17,6 +19,7 @@ public class Department {
 	private String name;
 	private Date established;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "department")
 	private List<Employee>employees;
 	
