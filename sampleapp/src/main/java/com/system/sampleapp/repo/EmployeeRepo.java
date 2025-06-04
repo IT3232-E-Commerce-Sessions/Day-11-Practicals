787b0dff5af10ec7098fa27b4354f3ee0d5282e8
Udara Nalawansa <udarakavishka13@gmail.com>
2025-06-04 23:39:24 +0530

    feat: add query methods for salary range and department ID retrieval in EmployeeRepo

--- sampleapp/src/main/java/com/system/sampleapp/repo/EmployeeRepo.java
+++ sampleapp/src/main/java/com/system/sampleapp/repo/EmployeeRepo.java
@@ -5,5 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.system.sampleapp.model.Employee;
 
 public interface EmployeeRepo extends JpaRepository<Employee, String> {
-
+    @Query("select e from employee e where e.salary between ?1 and ?2")
+	public List<Employee>findSalaryRange(int num1,int num2);
+	
+	@Query("select e from employee e where e.department_dept_id like '%?1'")
+	public List<Employee>getfromDeptID();
 }
